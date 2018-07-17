b54c31d51029267b1bd528dc73f946bb37902a5a
Petter Amland <petter@rain-games.com>
2018-07-17 19:26:54 +0200

    started on another rework.

--- sswg.py
+++ sswg.py
@@ -14,14 +14,9 @@ if len(sys.argv) > 1:
 images = glob('*.jpg')
 
 css = ''
-css = '<center>\n<body>'
-# css += '<html style="max-width: 50em; margin: auto;">'
-# css += '<div max-width="500px">'
-# css += '<body style="padding-left: 20%;>'
+css = '<left>\n<body>'
+css += '<html style="max-width: 800px; margin: auto;">'
 css += '<font color="#333333" face=""Century Gothic", CenturyGothic, AppleGothic, sans-serif" size="10em">\n'
-# css += '<br style="display: block; content: ""; margin-top: 0;" />'
-# css += '<br style="line-height:12px">'
-# css += '<body style="line-height:1em">'
 #
 # #images
 # for img in images:
@@ -38,30 +33,35 @@ with open(txt, 'r', encoding='utf-8') as t:
 
 
 text = text.replace('title:', '<h1 size="10em"><center> ', 1)
-# text = text.replace('heading:', '<font color="#222222" size="5em"><center>')
 text = text.replace('text:', '<font color="#222222" size="5em">')
-# text = text.replace('text:', '<font color="#222222" size="5em"><left>')
 
 text = text.replace('\n', '<br>\n')
 text = text.replace('  ', '&nbsp;&nbsp;')
 
 # delete commented lines
 new_text = ''
+tags = list()
+
 for line in text.split('\n'):
-    if not line.startswith('#'):
-        new_text += line
-        if line.startswith('<h3'):
-            new_text += '--------------------------------------</h3>'
-        if line.startswith('<h1'):
-            new_text += '</h1>'
-        # if line.startswith('<p'):
-        #     new_text += '</p>'
-        new_text += '\n'
+    if line.startswith('#'):
+        print('parse tag')
+        tag = line[1:]
+        if tag[0] == ' ':
+            tag = tag[1:]
+
+        # tag = line.split(' ')[1]
+        # if tag.startswith('#'):
+        #     tag = tag[1:]
+        # else:
+        #     tag = line.split(' ')[1]
+        print('TAG:', tag)
+    else:
+        new_text += line + '\n'
 
 text = new_text
 
 html_string = css + text
-print(html_string)
+# print(html_string)
 
 with open("index.html", "w", encoding='utf-8') as text_file:
     text_file.write(html_string)
