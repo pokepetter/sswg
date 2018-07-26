d32e0fe123b929875f5266847e2d2d0d95c905b7
Petter Amland <pokepetter@gmail.com>
2018-07-27 00:17:07 +0200

    trying to package this correctly...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sswg',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/pokepetter/sswg',
     author='Petter Amland',
     author_email='pokepetter@gmail.com',
