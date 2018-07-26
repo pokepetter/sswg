bf1c2a819df3ef1687798964a2ee73adf67ec75d
Petter Amland <pokepetter@gmail.com>
2018-07-27 00:11:54 +0200

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sswg',
-    version='1.0',
+    version='1.0.1',
     url='https://github.com/pokepetter/sswg',
     author='Petter Amland',
     author_email='pokepetter@gmail.com',
