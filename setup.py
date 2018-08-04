9a482c68055a55e3d0d1a39cb30953cb5d0665ef
Petter Amland <pokepetter@gmail.com>
2018-08-04 15:15:09 +0200

    giving up on pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sswg',
-    version='1.0.3',
+    version='1.0.0',
     url='https://github.com/pokepetter/sswg',
     author='Petter Amland',
     author_email='pokepetter@gmail.com',
