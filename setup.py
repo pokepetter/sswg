aad904348b45ae6648a5d324ad07f622387323a1
Petter Amland <pokepetter@gmail.com>
2018-09-16 14:52:15 +0200

    fixed setup script.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='sswg',
     version='1.0.0',
+    py_modules=['sswg'],
     url='https://github.com/pokepetter/sswg',
     author='Petter Amland',
     author_email='pokepetter@gmail.com',
